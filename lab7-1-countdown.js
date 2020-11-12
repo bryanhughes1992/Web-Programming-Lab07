de9f461762524e22567b934c117099244641c84f
bryannhughes <bryanhughes@hughesbuildingcompany.net>
2020-11-12 01:14:30 -0500

    Lab 07 - Part 1 Rough Copy

--- lab7-1-countdown.js
+++ lab7-1-countdown.js
@@ -1,29 +1,49 @@
 ﻿/* LAB 7-1 - FINAL COUNTDOWN!! */
 
 //create page load listener
-window.onload = pageReady;
+window.addEventListener("load", pageReady);
 
+/******************************
+	create page load function
+*******************************/
 function pageReady() {
-	//Variable for HTML body element
-	var body = document.getElementById('ex14Bod');
-	//Variable for HTML div named 'calendarBox'
-	var calendarBox = document.getElementById('calendarBox');
-	//Variable for HTML span element named 'timelabel'
-	var todaySpan = document.getElementsByClassName('timelabel');
-	//Variable for HTML span element named 'todayDate'
-	var dateSpan = document.getElementById('todayDate');
+
+	/************************************************
+		create variables for required HTML elements
+	*************************************************/
+
+	//Variable for HTML span element named 'todayData'
+	var dateSpan = document.getElementById('todayData');
 	//Variable for HTML span element named 'finalData'
-	var finalData = document.getElementById('')
+	var spanFinalData = document.getElementById('finalData');
+	//Variable for HTML span element named 'dueData'
+	var spanDueData = document.getElementById('dueData');
 
+	/***********************************************
+		create variables for now date and due date
+	************************************************/
 
+	//Instantiating a new Date() and storing it inside 'dateOne'
+	var dateOne = new Date();
+	//Setting a todaysDate variable to now in a string form
+	todaysDate = dateOne.toDateString();
+	//Setting the innerHTML to the 'todaysDate' variable
+	dateSpan.innerHTML = todaysDate;
+
+	//Instantiating a new Date() and initializing it to 11:59:59:59PM EST
+	var dateTwo = new Date(2020, 12, 15, 23, 59, 59, 59);
+	//Setting the innerHTML of the 'finalData' span to 'dateTwo' data
+	spanFinalData.innerHTML = dateTwo;
+
+	/*******************************************************
+		CONVERT TO UTC AND SUBTRACT TO GET TIME DIFFERENCE
+	********************************************************/
+
+	var daysUntilDue =
 }
-//create page load function
 
-	//create variables for required HTML elements
 
-	//create variables for now date and due date
 
-	//CONVERT TO UTC AND SUBTRACT TO GET TIME DIFFERENCE
 
 	//CONVERT TIME DIFFERENCE TO WHOLE NUMBER OF DAYS
 
