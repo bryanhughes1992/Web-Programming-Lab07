9875c2d38fec3e42145e546809f9f4e5ec946f35
bryannhughes <bryanhughes@hughesbuildingcompany.net>
2020-11-11 23:08:07 -0500

    Lab 07 - Upload

---
+++ lab7-1-countdown.js
@@ -0,0 +1,32 @@
+﻿/* LAB 7-1 - FINAL COUNTDOWN!! */
+
+//create page load listener
+window.onload = pageReady;
+
+function pageReady() {
+	//Variable for HTML body element
+	var body = document.getElementById('ex14Bod');
+	//Variable for HTML div named 'calendarBox'
+	var calendarBox = document.getElementById('calendarBox');
+	//Variable for HTML span element named 'timelabel'
+	var todaySpan = document.getElementsByClassName('timelabel');
+	//Variable for HTML span element named 'todayDate'
+	var dateSpan = document.getElementById('todayDate');
+	//Variable for HTML span element named 'finalData'
+	var finalData = document.getElementById('')
+
+
+}
+//create page load function
+
+	//create variables for required HTML elements
+
+	//create variables for now date and due date
+
+	//CONVERT TO UTC AND SUBTRACT TO GET TIME DIFFERENCE
+
+	//CONVERT TIME DIFFERENCE TO WHOLE NUMBER OF DAYS
+
+	//OUTPUT NOW DATE & DUE DATE TO HTML PAGE
+
+	//LOGIC TO CHECK IF DUE DATE HAS PASSED, AND OUPUT APPROPRIATE MESSAGE TO HTML PAGE
